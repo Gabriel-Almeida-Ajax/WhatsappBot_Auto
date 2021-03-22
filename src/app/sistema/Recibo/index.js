934b1a9aa54c5acecf2022b78d5ca9a84a01efe5
Gabriel Santos <58678638+Gabriel-Almeida-Ajax@users.noreply.github.com>
2021-03-22 15:47:33 -0300

    --- Auto-Git Commit ---

--- src/app/sistema/Recibo/index.js
+++ src/app/sistema/Recibo/index.js
@@ -60,14 +60,14 @@ async function send({ fnc, Client, props }) {
                         Files.errSend(fnc, `${props.Path}\\Recibo_`, `${props.Path}\\Recibo_${fnc.matricula}.pdf`)
                         console.error(`> Error in ${fnc.referencia} - ${fnc.matricula} - ${fnc.nome} - ${fnc.numero}`)
                         console.error('Error when sending sendContactVcardList: ', erro.text); //return object error
-                        resolve(error)
+                        resolve(erro)
                     });
             })
             .catch((erro) => {
                 Files.errSend(fnc, `${props.Path}\\Recibo_`, `${props.Path}\\Recibo_${fnc.matricula}.pdf`)
                 console.error(`> Error in ${fnc.referencia} - ${fnc.matricula} - ${fnc.nome} - ${fnc.numero}`)
                 console.error('Error when sending sendFile: ', erro.text); //return object error
-                resolve(error)
+                resolve(erro)
             });
     });
 
