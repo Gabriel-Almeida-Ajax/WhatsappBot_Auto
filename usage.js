a82cef52f3f2604e1695066ba382640672741c27
Gabriel Santos <58678638+Gabriel-Almeida-Ajax@users.noreply.github.com>
2021-03-22 23:28:57 -0300

    --- Auto-Git Commit ---

---
+++ usage.js
@@ -0,0 +1,35 @@
+
+const I = new Me()
+class You {
+    constructor() {
+        [...everything]
+    }
+}
+class Me extends You {
+    constructor(Life) {
+        this.having = "everything time";
+        this.expire = never;
+        this.distance = minimal;
+    }
+    Hug() {
+    }
+    Want() {
+    }
+}
+
+
+new Data(all).forEach((time) => {
+
+    meJust( I.Want ( Hug ( You ).forNever( let, you )));
+
+    meJust(to.be(happy).struggling(() => {
+        while (I.having == true) {
+            I.Want(() => {
+                sleep ( and ( wake.up.next ).to.you ).then(() =>{
+                    make(plans.and( make( dreams.come.true( "with you" ) ) ) )  
+                })
+            })
+        }
+    }))
+
+})
