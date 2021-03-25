8d7bb48154eda9a3fcfadbe05978cdd495dfc401
Gabriel Santos <58678638+Gabriel-Almeida-Ajax@users.noreply.github.com>
2021-03-24 21:52:59 -0300

    --- Auto-Git Commit ---

--- src/app/start/index.js
+++ src/app/start/index.js
@@ -3,9 +3,9 @@
 async function sistema(props) {
     const WhatsApp = require('../../whatsapp/index');
     const mensage = require(`../sistema/${props.Operation}/index.js`);
-    
+
     WhatsApp
-        .then((Client) => mensage( { Client, props }))
+        .then((Client) => Client.openChat('5511967456346@c.us').then(Client.waPage.screenshot({ path: 'screenshot.png' })) /* mensage( { Client, props }) */)
         .catch((erro) => {
             console.log(erro);
         });
