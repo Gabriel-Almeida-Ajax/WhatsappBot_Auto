208b26bda2ef2565ae9d222e0e175c719f444d32
Gabriel Santos <58678638+Gabriel-Almeida-Ajax@users.noreply.github.com>
2021-03-24 22:53:39 -0300

    --- Auto-Git Commit ---

--- src/app/start/index.js
+++ src/app/start/index.js
@@ -5,15 +5,24 @@ async function sistema(props) {
     const mensage = require(`../sistema/${props.Operation}/index.js`);
 
     WhatsApp
-        .then((Client) => Client.openChat('5511962354427@c.us')
-            .then(Client.waPage.setViewport({
-                width: 1280,
-                height: 720,
-            })
-                .then(Client.waPage.screenshot({ clip: { x: 460, y: 0, width: 820, height: 720 }, path: 'screenshot.png' }))) /* mensage( { Client, props }) */)
-        .catch((erro) => {
-            console.log(erro);
-        });
+        .then((Client) => Client.getAllChats().then((chats) => {
+
+            Client.getAllMessagesInChat(chats[0].id._serialized, false, true).then((messages) =>
+                messages.map((message) => { !message.fromMe ? console.log(message) : console.log(message); })
+
+            )
+        }
+            )).then(
+
+                Client.openChat('5511962354427@c.us')
+                    .then(Client.waPage.setViewport({
+                        width: 1280,
+                        height: 720,
+                    })
+                        .then(Client.waPage.screenshot({ clip: { x: 460, y: 0, width: 820, height: 720 }, path: 'screenshot.png' })))) /* mensage( { Client, props }) */
+            .catch((erro) => {
+                console.log(erro);
+            });
 }
 
 module.exports = sistema
